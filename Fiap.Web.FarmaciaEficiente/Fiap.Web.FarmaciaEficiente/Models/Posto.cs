55661c232270afbcf65916ba8dd86696e0e5cda5
luan.reis <luan.reis@portoseguro.com.br>
2023-11-16 23:10:54 -0300

    Feature: Relacionamentos e DB - Okay

---
+++ Fiap.Web.FarmaciaEficiente/Fiap.Web.FarmaciaEficiente/Models/Posto.cs
@@ -0,0 +1,30 @@
+﻿using Fiap.Web.FarmaciaEficiente.Models.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fiap.Web.FarmaciaEficiente.Models
+{
+    [Table("Posto")]
+    public class Posto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PostoId { get; set; }
+
+        /*"OneToOne"*/
+        [Required(ErrorMessage = "Endereco é obrigatório")]
+        public Endereco Endereco { get; set; }
+
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "O nome do posto de saúde deve ter entre 1 e 50 caracteres")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "A descrição do posto de saúde deve ter entre 1 e 50 caracteres")]
+        [Required(ErrorMessage = "Descrição é obrigatória")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Status é obrigatório")]
+        [EnumDataType(typeof(StatusEnum))]
+        public StatusEnum Status { get; set; } = StatusEnum.ATIVO;
+    }
+}
---
+++ Fiap.Web.FarmaciaEficiente/Fiap.Web.FarmaciaEficiente/Models/Retirada.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fiap.Web.FarmaciaEficiente.Models
+{
+    [Table("Retirada")]
+    public class Retirada
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RetiradaId { get; set; }
+
+        /*ManyToOne*/
+        [Required(ErrorMessage = "Estoque é obrigatório")]
+        public Estoque Estoque { get; set; }
+
+        /*Propriedade para representar a chave estrangeira de Usuario*/
+        [Required(ErrorMessage = "Usuário é obrigatório")]
+        public int UsuarioId { get; set; }
+
+        [ForeignKey("UsuarioId")] // Define que UsuarioId é a chave estrangeira
+        public Usuario Usuario { get; set; }
+
+        [Column(TypeName = "TIMESTAMP")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+    }
+}
